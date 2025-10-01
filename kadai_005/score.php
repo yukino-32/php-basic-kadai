84ffcaadb7ea417ea0713cb375aab6a55444abb9
yukino-32 <chino1124@icloud.com>
2025-10-01 14:32:43 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -21,7 +21,7 @@
      $score10 = '60';
 
      $total = $score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
-     echo $average = $total/10
+     echo $average = $total/10;
     ?>
 
 </body>
