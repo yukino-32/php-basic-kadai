fc43069ffdacf44590c0d938219859c1d2c92783
yukino-32 <chino1124@icloud.com>
2025-10-06 22:31:16 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <?php
+     $menu_data = ['name' =>'onion', 'price' => 200, 'weight' => '160'];
+
+     print_r($menu_data);
+
+    ?>
+
+</body>
+
+</html>
